1185694b02f872e3de28e72573dca38a276c9b6b
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-28 14:36:10 +0530

    Convert Users comp to class based comp

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,37 +1,50 @@
-import { useState } from 'react';
-import User from './User';
+import { useState, Component } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: false,
+      otherStates: "", // just for demo purpose
+    };
+  }
+  toggleUsersHandler() {
+    //Access prevValue like func comps
+    this.setState((prevState) => {
+      return { showUsers: !prevState.showUsers };
+    });
+  }
+  render() {
+    //Helper code/func just like func comps
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => {
+          return <User key={user.id} name={user.name} />;
+        })}
+      </ul>
+    );
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
-
+    // Return code will get rendered
+    return (
+      <div className={classes.users}>
+        {/* this.toggleUsersHandler needs bind(this) because else the first "this"
+      will refer to the parent i.e the button and not the class. So we need
+      the bind method */}
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
 export default Users;
