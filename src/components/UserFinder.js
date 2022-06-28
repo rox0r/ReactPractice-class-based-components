881c4500011e2e7a538eb2df41207b910c16071e
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-28 16:07:30 +0530

    Add class based Userfinder comp and use componentDidUpdate to change state of filteredUser

---
+++ src/components/UserFinder.js
@@ -0,0 +1,50 @@
+import { Fragment, Component } from "react";
+
+import classes from "./UserFinder.module.css";
+
+import Users from "./Users";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    //If Condition below is similar to useEffect dependency
+    if (this.state.searchTerm !== prevState.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+export default UserFinder;
